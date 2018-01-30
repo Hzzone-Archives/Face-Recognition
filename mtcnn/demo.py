5db39ef0c78cecbcf63d60889a2c550baa0f5688
Hzzone <Hzzone@outlook.com>
2018-01-30 18:11:50 +0800

    update

--- mtcnn/demo.py
+++ mtcnn/demo.py
@@ -1,13 +1,20 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import _init_paths
+import sys
+sys.path.insert(0, "/Users/HZzone/caffe/python")
 import caffe
 import cv2
 import numpy as np
-#from python_wrapper import *
 import os
 
+### 具体流程
+'''
+（1）构建金字塔输入到网络。
+（2）对金字塔的每一层，通过CNN自动滑动窗口，这是全卷积神经网络，没有全连接层，有stride，然后保存输出的人脸的窗口位置和概率。
+（3）将人脸窗口映射到原图img中的人脸位置，概率不变。
+（4）NMS。
+ (5) 2，3网络没有第1，2操作，输出的bounding box 都要对齐、补全成一个正方形
+（6）在原图画出人脸位置。
+'''
 def bbreg(boundingbox, reg):
     reg = reg.T 
     
@@ -81,18 +88,7 @@ def pad(boxesA, w, h):
     edx = np.maximum(0, edx-1)
     ey = np.maximum(0, ey-1)
     ex = np.maximum(0, ex-1)
-    
-    #print "dy"  ,dy 
-    #print "dx"  ,dx 
-    #print "y "  ,y 
-    #print "x "  ,x 
-    #print "edy" ,edy
-    #print "edx" ,edx
-    #print "ey"  ,ey 
-    #print "ex"  ,ex 
 
-
-    #print 'boxes', boxes
     return [dy, edy, dx, edx, y, ey, x, ex, tmpw, tmph]
 
 
@@ -113,6 +109,8 @@ def rerec(bboxA):
     return bboxA
 
 
+### 传入得分再加上scale
+###
 def nms(boxes, threshold, type):
     """nms
     :boxes: [:,0:5]
@@ -131,6 +129,7 @@ def nms(boxes, threshold, type):
     I = np.array(s.argsort()) # read s using I
     
     pick = [];
+    ### 循环直到没有重复的框
     while len(I) > 0:
         xx1 = np.maximum(x1[I[-1]], x1[I[0:-1]])
         yy1 = np.maximum(y1[I[-1]], y1[I[0:-1]])
@@ -148,6 +147,7 @@ def nms(boxes, threshold, type):
     return pick
 
 
+### out['prob1'][0,1,:,:], out['conv4-2'][0], scale, threshold[0]
 def generateBoundingBox(map, reg, scale, t):
     stride = 2
     cellsize = 12
@@ -156,45 +156,27 @@ def generateBoundingBox(map, reg, scale, t):
     dy1 = reg[1,:,:].T
     dx2 = reg[2,:,:].T
     dy2 = reg[3,:,:].T
+    ### 选出得分大于阈值的候选框
     (x, y) = np.where(map >= t)
 
-    yy = y
-    xx = x
-    
-    '''
-    if y.shape[0] == 1: # only one point exceed threshold
-        y = y.T
-        x = x.T
-        score = map[x,y].T
-        dx1 = dx1.T
-        dy1 = dy1.T
-        dx2 = dx2.T
-        dy2 = dy2.T
-        # a little stange, when there is only one bb created by PNet
-        
-        #print "1: x,y", x,y
-        a = (x*map.shape[1]) + (y+1)
-        x = a/map.shape[0]
-        y = a%map.shape[0] - 1
-        #print "2: x,y", x,y
-    else:
-        score = map[x,y]
-    '''
-    #print "dx1.shape", dx1.shape
-    #print 'map.shape', map.shape
-   
-
     score = map[x,y]
+    print "generateBoundingBox: "
+    print "x", x
+    print "y", y
+    print "score", score
+    ### 所有的候选框, 你看输出有很多个
     reg = np.array([dx1[x,y], dy1[x,y], dx2[x,y], dy2[x,y]])
 
     if reg.shape[0] == 0:
         pass
-    boundingbox = np.array([yy, xx]).T
+    boundingbox = np.array([y, x]).T
 
+    ### 窗体滑动
     bb1 = np.fix((stride * (boundingbox) + 1) / scale).T # matlab index from 1, so with "boundingbox-1"
     bb2 = np.fix((stride * (boundingbox) + cellsize - 1 + 1) / scale).T # while python don't have to
     score = np.array([score])
 
+    ### 就把所有的候选框再加上得分返回，窗体滑动的位置
     boundingbox_out = np.concatenate((bb1, bb2, score, reg), axis=0)
 
 
@@ -202,23 +184,16 @@ def generateBoundingBox(map, reg, scale, t):
 
 
 
+### 最后绘图
 def drawBoxes(im, boxes):
     x1 = boxes[:,0]
     y1 = boxes[:,1]
     x2 = boxes[:,2]
     y2 = boxes[:,3]
-    print "box shape", boxes.shape
     for i in range(x1.shape[0]):
         cv2.rectangle(im, (int(x1[i]), int(y1[i])), (int(x2[i]), int(y2[i])), (0,255,0), 1)
-        print "box index", (int(x1[i]), int(y1[i])), (int(x2[i]), int(y2[i]))
     return im
 
-from time import time
-_tstart_stack = []
-def tic():
-    _tstart_stack.append(time())
-def toc(fmt="Elapsed: %s s"):
-    print fmt % (time()-_tstart_stack.pop())
 
 
 def detect_face(img, minsize, PNet, RNet, ONet, threshold, fastresize, factor):
@@ -263,8 +238,11 @@ def detect_face(img, minsize, PNet, RNet, ONet, threshold, fastresize, factor):
         PNet.blobs['data'].reshape(1, 3, ws, hs)
         PNet.blobs['data'].data[...] = im_data
         out = PNet.forward()
-    
+        print "scale: ", scale
+        print "prob: ", out['prob1'][0,1,:,:].shape
+        print "boundingbox: ", out['prob1'][0,1,:,:].shape
         boxes = generateBoundingBox(out['prob1'][0,1,:,:], out['conv4-2'][0], scale, threshold[0])
+        ### 这里就用bounding box， 判断一下有没有脸再做
         if boxes.shape[0] != 0:
 
             pick = nms(boxes, 0.5, 'Union')
@@ -273,6 +251,7 @@ def detect_face(img, minsize, PNet, RNet, ONet, threshold, fastresize, factor):
                 boxes = boxes[pick, :]
 
         if boxes.shape[0] != 0:
+            ### 这里就把所有scale的候选框都保存到total_boxes里面
             total_boxes = np.concatenate((total_boxes, boxes), axis=0)
          
 
@@ -281,9 +260,9 @@ def detect_face(img, minsize, PNet, RNet, ONet, threshold, fastresize, factor):
         # nms
         pick = nms(total_boxes, 0.7, 'Union')
         total_boxes = total_boxes[pick, :]
-        print "[2]:",total_boxes.shape[0]
         
         # revise and convert to square
+        ### 因为后两个网络有全连接层，所以必须弄到正方形
         regh = total_boxes[:,3] - total_boxes[:,1]
         regw = total_boxes[:,2] - total_boxes[:,0]
         t1 = total_boxes[:,0] + total_boxes[:,5]*regw
@@ -293,14 +272,13 @@ def detect_face(img, minsize, PNet, RNet, ONet, threshold, fastresize, factor):
         t5 = total_boxes[:,4]
         total_boxes = np.array([t1,t2,t3,t4,t5]).T
         total_boxes = rerec(total_boxes) # convert box to square
-        print "[4]:",total_boxes.shape[0]
         
         total_boxes[:,0:4] = np.fix(total_boxes[:,0:4])
-        print "[4.5]:",total_boxes.shape[0]
         [dy, edy, dx, edx, y, ey, x, ex, tmpw, tmph] = pad(total_boxes, w, h)
 
 
     numbox = total_boxes.shape[0]
+    ### 把候选框输入到第二个网络 这里就和第一个网络同样的操作，只不过没有构造图像金字塔
     if numbox > 0:
         # second stage
 
@@ -332,7 +310,6 @@ def detect_face(img, minsize, PNet, RNet, ONet, threshold, fastresize, factor):
         
         score =  np.array([score[pass_t]]).T
         total_boxes = np.concatenate( (total_boxes[pass_t, 0:4], score), axis = 1)
-        print "[5]:",total_boxes.shape[0]
 
         
         mv = out['conv5-2'][pass_t, :].T
@@ -341,17 +318,14 @@ def detect_face(img, minsize, PNet, RNet, ONet, threshold, fastresize, factor):
             #print 'pick', pick
             if len(pick) > 0 :
                 total_boxes = total_boxes[pick, :]
-                print "[6]:",total_boxes.shape[0]
                 total_boxes = bbreg(total_boxes, mv[:, pick])
-                print "[7]:",total_boxes.shape[0]
                 total_boxes = rerec(total_boxes)
-                print "[8]:",total_boxes.shape[0]
             
         #####
         # 2 #
         #####
-        print "2:",total_boxes.shape
 
+        ### 第三步就输出关键点，同时对应到每个bounding box
         numbox = total_boxes.shape[0]
         if numbox > 0:
             # third stage
@@ -380,7 +354,6 @@ def detect_face(img, minsize, PNet, RNet, ONet, threshold, fastresize, factor):
             points = points[pass_t, :]
             score = np.array([score[pass_t]]).T
             total_boxes = np.concatenate( (total_boxes[pass_t, 0:4], score), axis=1)
-            print "[9]:",total_boxes.shape[0]
             
             mv = out['conv6-2'][pass_t, :].T
             w = total_boxes[:,3] - total_boxes[:,1] + 1
@@ -391,61 +364,19 @@ def detect_face(img, minsize, PNet, RNet, ONet, threshold, fastresize, factor):
 
             if total_boxes.shape[0] > 0:
                 total_boxes = bbreg(total_boxes, mv[:,:])
-                print "[10]:",total_boxes.shape[0]
                 pick = nms(total_boxes, 0.7, 'Min')
                 
                 #print pick
                 if len(pick) > 0 :
                     total_boxes = total_boxes[pick, :]
-                    print "[11]:",total_boxes.shape[0]
                     points = points[pick, :]
-
-    #####
-    # 3 #
-    #####
-    print "3:",total_boxes.shape
 
     return total_boxes, points
 
 
-
-
-    
-def initFaceDetector():
-    minsize = 20
-    caffe_model_path = "/home/duino/iactive/mtcnn/model"
-    threshold = [0.6, 0.7, 0.7]
-    factor = 0.709
-    caffe.set_mode_cpu()
-    PNet = caffe.Net(caffe_model_path+"/det1.prototxt", caffe_model_path+"/det1.caffemodel", caffe.TEST)
-    RNet = caffe.Net(caffe_model_path+"/det2.prototxt", caffe_model_path+"/det2.caffemodel", caffe.TEST)
-    ONet = caffe.Net(caffe_model_path+"/det3.prototxt", caffe_model_path+"/det3.caffemodel", caffe.TEST)
-    return (minsize, PNet, RNet, ONet, threshold, factor)
-
-def haveFace(img, facedetector):
-    minsize = facedetector[0]
-    PNet = facedetector[1]
-    RNet = facedetector[2]
-    ONet = facedetector[3]
-    threshold = facedetector[4]
-    factor = facedetector[5]
-    
-    if max(img.shape[0], img.shape[1]) < minsize:
-        return False, []
-
-    img_matlab = img.copy()
-    tmp = img_matlab[:,:,2].copy()
-    img_matlab[:,:,2] = img_matlab[:,:,0]
-    img_matlab[:,:,0] = tmp
-    
-    #tic()
-    boundingboxes, points = detect_face(img_matlab, minsize, PNet, RNet, ONet, threshold, False, factor)
-    #toc()
-    containFace = (True, False)[boundingboxes.shape[0]==0]
-    return containFace, boundingboxes
-
 def main():
     imglistfile = "imglist.txt"
+    ### 人脸最小的大小
     minsize = 20
 
     caffe_model_path = "./model"
@@ -459,30 +390,24 @@ def main():
     ONet = caffe.Net(caffe_model_path+"/det3.prototxt", caffe_model_path+"/det3.caffemodel", caffe.TEST)
 
 
-    #error = []
-    f = open(imglistfile, 'r')
-    for imgpath in f.readlines():
-        imgpath = imgpath.split('\n')[0]
-        print "######\n", imgpath
-        img = cv2.imread(imgpath)
-        img_matlab = img.copy()
-        tmp = img_matlab[:,:,2].copy()
-        img_matlab[:,:,2] = img_matlab[:,:,0]
-        img_matlab[:,:,0] = tmp
+    with open(imglistfile, 'r') as f:
+        for imgpath in f.readlines():
+            imgpath = imgpath.split('\n')[0]
+            print "######\n", imgpath
+            img = cv2.imread(imgpath)
+            img_matlab = img.copy()
+            tmp = img_matlab[:,:,2].copy()
+            img_matlab[:,:,2] = img_matlab[:,:,0]
+            img_matlab[:,:,0] = tmp
 
-        # check rgb position
-        #tic()
-        boundingboxes, points = detect_face(img_matlab, minsize, PNet, RNet, ONet, threshold, False, factor)
-        #toc()
+            # check rgb position
+            #tic()
+            boundingboxes, points = detect_face(img_matlab, minsize, PNet, RNet, ONet, threshold, False, factor)
+            #toc()
 
-        img = drawBoxes(img, boundingboxes)
-        cv2.imshow('img', img)
-        ch = cv2.waitKey(0) & 0xFF
-        if ch == 27:
-            break
-
-    #print error
-    f.close()
+            img = drawBoxes(img, boundingboxes)
+            cv2.imshow('xiaoxiongwoaini', img)
+            cv2.waitKey(0)
 
 if __name__ == "__main__":
     main()
